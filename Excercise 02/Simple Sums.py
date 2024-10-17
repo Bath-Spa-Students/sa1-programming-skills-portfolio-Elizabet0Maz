2ebd2e4cad2ca67dba3ba8cb2ac7c01930ef71c5
Elizabet0Maz <elizabethmaznychenko@gmail.com>
2024-10-17 10:46:12 +0400

    Create Simple Sums.py
    Simple Sums

---
+++ Excercise 02/Simple Sums.py
@@ -0,0 +1,10 @@
+# Exercise Simple Sums allows student to learn how to preform calculations by means of variables. It helps to make code arranged, and add more readability to it.
+# It allows student to preform calculations of two variables inside of another variable, that allows to make code shorter and simple.
+# First Integer.
+Integer1 = 8
+# Second Integer.
+Integer2 = 10
+# The Variable where the calculation is preformed of First and Second Variables where the Integers are stored.
+Addition = (Integer1 + Integer2)
+# The output of the Variable where the sum of two variables are stored.
+print(Addition)

600cc7e3d32461646a6272aeafcb04712bb1c0b6
Elizabet0Maz <elizabethmaznychenko@gmail.com>
2024-11-20 12:55:01 +0400

    Simple Search.py

--- Excercise 08/Simple Search.py
+++ Excercise 08/Simple Search.py
@@ -1,7 +1,7 @@
 # Write a program that searches for a specific string within a list of strings. 
 # The list is initialized with specific names ("Jake" "Zac", "Ian", "Ron", "Sam", "Dave"). , and your task is to search for "Sam".
 
-# List of the names.
+# List of the names. 
 Names = ["Jake","Zac","Ian","Ron","Sam","Dave"]
 # Print Function
 print(Names)

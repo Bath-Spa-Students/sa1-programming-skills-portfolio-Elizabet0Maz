18f2069749da0d5699ce309e162b366052db0c63
Elizabet0Maz <elizabethmaznychenko@gmail.com>
2024-11-20 14:55:54 +0400

    Dictionaries.py

---
+++ z_Bonus_Exercises/Dictionaries.py
@@ -0,0 +1,7 @@
+# Dictionary
+Dictionary = {"First_Name" : "Elizabet",
+              "Last_Name" :  "Maznychenko",
+              "City" : "Kyiv",
+              "Age" : 19}
+# Print Function
+print("First_Name:", Dictionary["First_Name"], "\nLast_Name:", Dictionary["Last_Name"], "\nCity:", Dictionary["City"], "\nAge:", Dictionary["Age"])

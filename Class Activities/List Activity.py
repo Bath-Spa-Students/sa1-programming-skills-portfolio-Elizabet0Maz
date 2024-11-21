84094c75d7ddf0c3ce51a19d99449d4243371b56
Elizabet0Maz <elizabethmaznychenko@gmail.com>
2024-11-21 10:00:31 +0400

    List Activity.py

---
+++ Class Activities/List Activity.py
@@ -0,0 +1,15 @@
+# Homogeneous List
+Name = input("Insert your name please: ")
+List1 = ["Hello", "This is", "Welcome"]
+print(List1[0])
+print(List1)
+
+# Hetrogeneous List
+List2 = ["How are you", Name, 34, 23.3]
+print(List2)
+print(List2[1])
+
+# Repitiion Operator
+List3 = [2, 4, 8, 16]
+Sub_List = List3 * 4
+print(Sub_List)

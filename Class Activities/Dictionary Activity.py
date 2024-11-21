cb9ed678ef718a02b4508b5288edf7a545da86ed
Elizabet0Maz <elizabethmaznychenko@gmail.com>
2024-11-21 09:53:08 +0400

    Dictionary Activity.py

---
+++ Class Activities/Dictionary Activity.py
@@ -0,0 +1,7 @@
+dictionary = {"Drink" : "Cola", 
+              "Snack" : "Chips", 
+              "Credit" : "20"}
+print(dictionary.pop("Credit"))
+print(dictionary.keys())
+print(dictionary.values())
+print(dictionary.items())
